2172aadf9ee6c167bbd21f94d12ee42622b8db69
KentTan <kenttan@seekasia.com>
2017-11-08 17:17:40 +0800

    add qr code scanner

--- app/src/main/java/pomelo/kenttan/shadowphone/QRScannerActivity.java
+++ app/src/main/java/pomelo/kenttan/shadowphone/QRScannerActivity.java
@@ -2,12 +2,43 @@ package pomelo.kenttan.shadowphone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class QRScannerActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class QRScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private static final String TAG = QRScannerActivity.class.getSimpleName();
+    private ZXingScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qrscanner);
+
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Log.v(TAG, rawResult.getText()); // Prints scan results
+        Log.v(TAG, rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        // If you would like to resume scanning, call this method below:
+        // mScannerView.resumeCameraPreview(this);
     }
 }
